99fb212d97fffd02f0373212db23617505b8ede6
Chen,Chun-Yu <yo80106@gmail.com>
2017-04-25 10:27:31 +0800

    Little update.

--- String Processing/bottleneck_format.R
+++ String Processing/bottleneck_format.R
@@ -1,7 +1,13 @@
-test = read.csv("~/Downloads/cw22type123.csv", header = FALSE, stringsAsFactors = FALSE)
-str(test)
+if(sum(installed.packages(.libPaths())[,1] %in% "stringr") == 0){
+    install.packages("stringr")
+    library(stringr)
+}else{
+    library(stringr)
+}
+args = commandArgs(trailingOnly = TRUE)
+
+test = read.csv(args[1], header = FALSE, stringsAsFactors = FALSE)
 head(test)
-library(stringr)
 for(i in 2:ncol(test)){
     test[,i] = gsub("\\?", 00, test[,i])
     test[,i] = str_pad(test[,i], width =2, side="left", pad=0)
@@ -12,4 +18,4 @@ for(t in seq(2,21,2)){
 }
 colnames(new) = c("POP", 1:10)
 new[,1] = paste0(test[,1],",")
-write.table(new, file = "~/Desktop/angela.txt", sep = " ", quote = FALSE, row.names = FALSE)
+write.table(new, file = args[2], sep = " ", quote = FALSE, row.names = FALSE)
